a631abfe349b488442d57493fe5648af3603265a
Daniel Carpenter <dlcarpenter@danielcarpenter.net>
2014-10-08 22:45:45 -0500

    Making the names of the getters and setters more verbose and adding documentation. Thinking we will need to be accessing this class a lot throughout the project so it will be helpful to have everything spelled out in front of us.

--- CleanSweep/src/DePaul/SE459/CleanSweep/Tile.java
+++ CleanSweep/src/DePaul/SE459/CleanSweep/Tile.java
@@ -10,88 +10,177 @@ public class Tile
     private int ds;
     private boolean cs;
     
-    public Tile(int xC, int yC, int sC, int rC, int lC, int uC, int dC, int dsC, boolean csC)
+    public Tile(int xPos, int yPos, int surface, int right, int left, int up, int down, int dirt, boolean charging)
     {
-        setX(xC);
-        setY(yC);
-        setSs(sC);
-        setR(rC);
-        setL(lC);
-        setU(uC);
-        setD(dC);
-        setDs(dsC);
-        setCs(csC);
+        setX(xPos);
+        setY(yPos);
+        setSurfaceType(surface);
+        setRightPath(right);
+        setLeftPath(left);
+        setUpperPath(up);
+        setLowerPath(down);
+        setDirtAmount(dirt);
+        setChargingStation(charging);
     }
     
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setX(int xPos) {
+        this.x = xPos;
     }
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setY(int yPos) {
+        this.y = yPos;
     }
 
-    public int getSs() {
+    /**
+     * @return The surface type of the Tile.
+     * 1 = bare floor
+     * 2 = low-pile carpet
+     * 4 = high-pile carpet
+     */
+    public int getSurfaceType() {
         return ss;
     }
 
-    public void setSs(int ss) {
-        this.ss = ss;
+    /**
+     * Sets the surface type of the Tile.
+     * 1 = bare floor
+     * 2 = low-pile carpet
+     * 4 = high-pile carpet
+     * @param surfaceType
+     */
+    public void setSurfaceType(int surfaceType) {
+        this.ss = surfaceType;
     }
 
-    public int getR() {
+    /**
+     * @return The path of the Tile to the right of this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     */
+    public int getRightTilePath() {
         return r;
     }
 
-    public void setR(int r) {
-        this.r = r;
+    /**
+     * Sets the path of the Tile to the right of this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     * @param rightPath
+     */
+    public void setRightPath(int rightPath) {
+        this.r = rightPath;
     }
 
-    public int getL() {
+    /**
+     * @return The path of the Tile to the left of this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     */
+    public int getLeftPath() {
         return l;
     }
 
-    public void setL(int l) {
-        this.l = l;
+    /**
+     * Sets the path of the Tile to the left of this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     * @param leftPath
+     */
+    public void setLeftPath(int leftPath) {
+        this.l = leftPath;
     }
 
-    public int getU() {
+    /**
+     * @return The path of the Tile above this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     */
+    public int getUpperPath() {
         return u;
     }
 
-    public void setU(int u) {
-        this.u = u;
+    /**
+     * Sets the path of the Tile above this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     * @param up
+     */
+    public void setUpperPath(int up) {
+        this.u = up;
     }
 
-    public int getD() {
+    /**
+     * @return The path of the Tile below this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     */
+    public int getLowerPath() {
         return d;
     }
 
-    public void setD(int d) {
-        this.d = d;
+    /**
+     * Sets the path of the Tile below this Tile.
+     * 0 = unknown
+     * 1 = open
+     * 2 = obstacle
+     * 4 = stairs
+     * @param down
+     */
+    public void setLowerPath(int down) {
+        this.d = down;
     }
 
-    public int getDs() {
+    /**
+     * TODO: Since there is a requirement that the Clean Sweep not know how dirty something is,
+     * should this return a boolean, true if ds > 0, false if ds = 0?
+     * @return The amount of dirt in the Tile.
+     */
+    public int getDirtAmount() {
         return ds;
     }
 
-    public void setDs(int ds) {
-        this.ds = ds;
+    /**
+     * Sets the amount of dirt present in the Tile.
+     * @param dirtAmount
+     */
+    public void setDirtAmount(int dirtAmount) {
+        this.ds = dirtAmount;
     }
 
-    public boolean isCs() {
+    /**
+     * @return True if this Tile contains the charging station, false if it does not.
+     */
+    public boolean isChargingStation() {
         return cs;
     }
 
-    public void setCs(boolean cs) {
-        this.cs = cs;
+    /**
+     * Sets whether or not this Tile contains the charging station.
+     * @param isCs
+     */
+    public void setChargingStation(boolean isCs) {
+        this.cs = isCs;
     }
 }
